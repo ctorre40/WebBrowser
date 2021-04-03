78ecbd76b261021649d07432b74209bb18bae349
Casandra Torres Rebollar <czt0051@auburn.edu>
2021-04-03 14:21:18 -0500

    udated the history data set added windows forms

---
+++ WebBrowser.UI/HistoryManagerForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebBrowser.Logic;
+//using WebBrowser.HistoryDataSetTableAdapters;
+
+namespace WebBrowser.UI
+{
+    public partial class HistoryManagerForm : Form
+    {
+        public HistoryManagerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public static List<HistoryItem> GetHistoryItems()
+        {
+            var adapter = new HistoryTableAdapter();
+            var results = new List<HistoryItem>();
+            var rows = adapter.GetData();
+
+            foreach (var row in rows)
+            {
+                var hItem = new HistoryItem();
+                hItem.URL = row.URL;
+                hItem.Title = row.Title;
+                hItem.Date = row.Date;
+
+                results.Add(hItem);
+            }
+            return results;
+        }
+    }
+}
