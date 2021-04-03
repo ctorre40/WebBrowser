c11774d6ec4e0df6b7d86929a3348b5186a8fef2
Casandra Torres Rebollar <czt0051@auburn.edu>
2021-04-03 10:39:10 -0500

    created new classes under logic, added items to history item

---
+++ WebBrowser.Logic/HistoryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBrowser.Logic
+{
+    class HistoryItem
+    {
+        // need to decide whether this should be public or private
+        public string URL = "";
+        public string Title = "";
+        public DateTime Date;
+    }
+}
